dbc22c5f8b9fdfcf87e9460132993beca3c8c013
neerunaveenjakhar <58066244+neerunaveenjakhar@users.noreply.github.com>
2020-04-28 18:14:07 +0530

    Update OMDB.R

--- OMDB.R
+++ OMDB.R
@@ -1,19 +1,11 @@
 rm(list = ls())
-setwd("~/Dropbox/Edwisor/A6")
 library(tidyverse)
 library(stringr)
 library(curl)
 library(jsonlite)
-#library(XML)
 library(httr)
-#library(Omdbapi)
 library(usethis)
 
-#install.packages("usethis")
-#install.packages("listviewer")
-
-#OMDB_API_KEY=929a69ec
-#movie_key <- Sys.getenv("OMDB_API_KEY")
 
 request <- glue::glue("http://www.omdbapi.com/?t=Interstellar&y=2014&plot=short&r=xml&apikey=929a69ec")
 request
